bf7b290cd284a5ab1d97208b9a730b6867ff832e
bingyao zhu <bzhu@bingyaos-iMac.local>
2018-11-02 10:48:06 +0100

    reaaranged the buttons

--- js/wiki.editor.js
+++ js/wiki.editor.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-    $("textarea[name=article]").summernote();
+    $("textarea[name=article]").summernote({
+        toolbar: [
+            // [groupName, [list of button]]
+            ['style', ['fontname', 'bold', 'italic', 'underline', 'clear']],
+            ['font', ['strikethrough', 'superscript', 'subscript']],
+            ['fontsize', ['fontsize']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['height', ['height']],
+            ['insert', ['picture', 'link', 'table', 'hr']],
+            ['help', ['fullscreen','undo','redo','help']]
+        ]
+    });
 });
