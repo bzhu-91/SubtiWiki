3f159c51e73e6e2f009b48c4d648cba1c10f38f0
bingyao zhu <bzhu@bingyaos-iMac.local>
2018-11-05 16:07:59 +0100

    changes to wiki editor

--- js/wiki.editor.js
+++ js/wiki.editor.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $("textarea[name=article]").summernote({
         toolbar: [
             // [groupName, [list of button]]
-            ['style', ['fontname', 'bold', 'italic', 'underline', 'clear']],
+            ['style', ['style','fontname', 'bold', 'italic', 'underline', 'clear']],
             ['font', ['strikethrough', 'superscript', 'subscript']],
             ['fontsize', ['fontsize']],
             ['color', ['color']],
