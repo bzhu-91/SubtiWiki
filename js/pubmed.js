e0d443981ce2b20a5f2f03cf23ae174e0d47a53f
bingyao zhu <bzhu@bingyaos-iMac.local>
2018-11-30 12:45:37 +0100

    add support functiont to DOI blocks

--- js/pubmed.js
+++ js/pubmed.js
@@ -9,7 +9,30 @@ $(window).on("load", function(){
 		$(div).load("pubmed?ids=" + encodeURIComponent(ids), null);
 		$(div).html("Loading");
 	});
-	$(".pubmed").on("click", function(){
-		window.open("https://www.ncbi.nlm.nih.gov/pubmed/"+this.id);
+	
+	$("DOI").each(function(i, each){
+		var DOIs = each.innerHTML.trim().split(/\s+/g);
+		var div = $(each).parents(".m_array")[0];
+		div = div ? div : each.parentNode;
+		$(div).html("");
+		DOIs.forEach(function(doi){
+			ajax.get({
+				url: "https://crosscite.org/format?doi="+doi+"&style=cell&lang=en-US",
+			}).done(function(status, data, error, xhr){
+				if (status == 200) {
+					var box = $("<div></div>").html(data).addClass("pubmed").on("click", function(){
+						window.open("https://doi.org/" + doi);
+					});
+					$(div).append(box);
+				} else {
+					var box = $("<div></div>").html(data).addClass("pubmed")
+					$(div).append(box);
+				}
+			});
+		})
 	})
+});
+
+$(document).on("click", ".pubmed", function(){
+	window.open("https://www.ncbi.nlm.nih.gov/pubmed/"+this.id);
 });
